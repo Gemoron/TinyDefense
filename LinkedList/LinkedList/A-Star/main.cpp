5321a193f9b3732899cfee9acc57355b2454220d
Stefan Sanner <gemoron@hotmail.de>
2014-09-03 21:46:11 +0200

    Adds comments to code, base files for A-Star

---
+++ LinkedList/LinkedList/A-Star/main.cpp
@@ -0,0 +1,18 @@
+//A* main.cpp
+//Stefan Sanner
+//Entwicklung und Testen der LinkedElement Klasse
+#include <iostream>
+#include "LinkedElement.h"
+#include "LinkedList.h"
+
+using namespace std;
+
+
+void main(void)
+{
+
+	locale::global(locale("German_germany"));
+
+	
+	system("pause");
+}
